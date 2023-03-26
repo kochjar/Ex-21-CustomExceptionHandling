fa1d1a37aeab18d51f4d1036775b6a2d5262e161
Timotej Kochjar <tiko54096@edu.ucl.dk>
2023-03-26 23:10:38 +0200

    Add project files.

---
+++ CustomExceptionHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿using TotallySafeLib;
+namespace CustomExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int posValue = TotallySafe.GetValueAtPosition(-1);
+
+            Console.WriteLine(posValue);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ TotallySafeLib/TotallySafe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TotallySafeLib
+{
+    public class TotallySafe
+    {
+        public static double Divider(int number)
+        {
+            return 7 / number;
+        }
+        public static int StringToInt(string stringToConvert)
+        {
+            return int.Parse(stringToConvert);
+        }
+        public static int GetValueAtPosition(int positionInArray)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            return intArray[positionInArray];
+        }
+
+    }
+}
